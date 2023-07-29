42e7341750577b49ce50a9db0ed1dd7dc278b30b
runs-15 <harunsixsixfour@gmail.com>
2023-07-30 00:37:55 +0700

    added logic behind missing word(s)

--- quiz.py
+++ quiz.py
@@ -5,6 +5,12 @@ from PIL import Image, ImageFont
 def wrap(s, w):
     return [s[i:i + w] for i in range(0, len(s), w)]
 
+def remove_random_character(phrase, n_remove):
+    placeholder = random.sample(range(0, len(phrase)), n_remove)
+    for num in placeholder:
+        phrase = phrase[:num] + '_' + phrase[num + 1:]
+    return phrase, sorted(placeholder, reverse=True)
+
 # verse_key sections
 def get_random_verse_key():
     url = "https://api.quran.com/api/v4/verses/random"
@@ -62,6 +68,28 @@ def get_random_verse_data_from_juz(juz, qari):
 
     return verse_data
 
+def get_random_verse_data_from_chapter(chapter, qari):
+    url = f"https://api.quran.com/api/v4/verses/by_chapter/{chapter}"
+    querystring = {"words":"false", "per_page":"1"}
+    headers = {"Content-Type": "application/json"}
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    max_page = response.json()['pagination']['total_pages']
+    
+    querystring = {"words":"false", "audio":qari, "page":random.randint(1, max_page), "per_page":"1"}
+    headers = {"Content-Type": "application/json"}
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    verse_data = response.json()["verses"][0]
+    
+    if verse_data["audio"]['url'].startswith("mirrors.quranicaudio.com"):
+        verse_data["audio"]['url'] = "https://" + verse_data["audio"]['url']
+    elif verse_data["audio"]['url'].startswith("//mirrors.quranicaudio.com"):
+        verse_data["audio"]['url'] = verse_data["audio"]['url'].replace("//", "https://")
+    else:
+        verse_data["audio"]['url'] = "https://verses.quran.com/" + verse_data["audio"]['url']
+
+    return verse_data
+
 def get_glyph_image_from_verse_key(verse_key, filename="glyph", style="QFC", font_size=40, color=(255, 255, 255, 255)):
     url = "https://api.quran.com/api/v4/quran/verses/code_v2"
     querystring = {"verse_key":verse_key}
@@ -93,6 +121,55 @@ def get_glyph_image_from_verse_key(verse_key, filename="glyph", style="QFC", fon
         img.save(f"img/{filename}{i}.png")
         
     return i
+
+def get_uncompleted_glyph_image_from_verse_key(verse_key, filename="glyph", style="QFC", font_size=40, color=(255, 255, 255, 255)):
+    url = "https://api.quran.com/api/v4/quran/verses/code_v2"
+    querystring = {"verse_key":verse_key}
+    headers = {"Content-Type": "application/json"}
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    
+    
+    text = response.json()["verses"][0]['code_v2'][:-1].replace(" ", "")
+    
+    randomizer = random.randint(1, int(len(text)/4) if len(text) > 4 else 2)
+    
+    text2 = text
+    
+    text, index_hilang = remove_random_character(text, randomizer)
+    text = textwrap.wrap(text, 8)
+    
+    text3 = ""
+    for i in index_hilang:
+        text3 += text2[i]
+    
+    page_number = response.json()["verses"][0]['v2_page']
+    
+    if page_number >= 100:
+        pass
+    elif page_number >= 10:
+        page_number = "0" + str(page_number)
+    elif page_number < 10:
+        page_number = "00" + str(page_number)
+        
+    font_size = 30
+    font_filepath = f"https://cdn.rawgit.com/mustafa0x/qpc-fonts/f93bf5f3/mushaf-v2/QCF2{page_number}.ttf"
+    color = (255, 255, 255, 255)
+
+    for i, j in enumerate(text):
+        font = ImageFont.truetype(font_filepath, size=font_size)
+        mask_image = font.getmask(j[::-1], "L")
+        img = Image.new("RGBA", mask_image.size)
+        img.im.paste(color, (0, 0) + mask_image.size, mask_image)
+        img.save(f"img/{filename}{i}.png")
+        
+    
+    font = ImageFont.truetype(font_filepath, size=font_size)
+    mask_image = font.getmask(text3, "L")
+    img = Image.new("RGBA", mask_image.size)
+    img.im.paste(color, (0, 0) + mask_image.size, mask_image)
+    img.save(f"img/{filename}cropped.png")
+        
+    return i, randomizer
 
 # print(get_random_verse_data_from_juz(30, 12))
 
